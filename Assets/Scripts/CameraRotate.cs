9af0732c4262776646ea1c77a0bcc51a5ad82203
Jan-Cord <gnaarf@gmx.de>
2021-01-29 23:44:31 +0100

    ✨ added a CameraRotate-Behaviour - created CordsScene - CameraRotate.cs invokes a Coroutine that rotates around it's projected position (projected into xy-plane)

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    Coroutine coroutine;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && coroutine == null)
+        {
+            coroutine = StartCoroutine(TurnCamera(.5f));
+        }
+    }
+
+    IEnumerator TurnCamera(float duration)
+    {
+        Vector3 startPosition = transform.position;
+        Quaternion startRotation = transform.rotation;
+
+        float coroutineRunTime = 0f;
+        Vector3 rotationPoint = new Vector3(transform.position.x, transform.position.y, 0f);
+
+        while(coroutineRunTime < duration)
+        {
+            coroutineRunTime += Time.deltaTime;
+            transform.RotateAround(rotationPoint, Vector3.up, Time.deltaTime / duration * 180);
+            yield return null; 
+        }
+
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+        transform.RotateAround(rotationPoint, Vector3.up, 180);
+
+        coroutine = null;
+    }
+}
